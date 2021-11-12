cd48681bc6930a060df14c01b7ea00456aa77b8a
Настя Кобельник <neongraveston@gmail.com>
2021-11-12 20:03:52 +0200

    Initial commit

---
+++ GraphicPlotter/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace GraphicPlotter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GraphicPlotter.Properties.Resources.function;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 112);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(443, 9);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Color = System.Drawing.Color.Green;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(346, 115);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ GraphicPlotter/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicPlotter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            one();
+            line();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            one();
+            line();
+        }
+
+        private void one()
+        {
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.White);
+            Pen a = new Pen(Color.Blue, 1);
+            Pen b = new Pen(Color.Green, 2);
+            Font drawFont = new Font("Arial", 12);
+            Font signatureFont = new Font("Arial", 7);
+            SolidBrush drawBrush = new SolidBrush(Color.Blue);
+            StringFormat drawFormat = new StringFormat();
+            drawFormat.FormatFlags = StringFormatFlags.DirectionRightToLeft;
+
+            int sizeWidth = 900;
+            int sizeHeight = 600;
+
+            Point center = new Point(((int)(sizeWidth / 2) - 8), (int)((sizeHeight / 2) - 19));
+            g.DrawLine(a, 10, center.Y, center.X, center.Y);
+            g.DrawLine(a, center.X, center.Y, 2 * center.X - 10, center.Y);
+            g.DrawLine(a, center.X, 10, center.X, center.Y);
+            g.DrawLine(a, center.X, center.Y, center.X, 2 * center.Y - 10);
+            g.DrawString("X", drawFont, drawBrush, new PointF(2 * center.X - 5, center.Y + 10), drawFormat);
+            g.DrawString("Y", drawFont, drawBrush, new PointF(center.X + 30, 5), drawFormat);
+            g.DrawString("0", signatureFont, drawBrush, new PointF(center.X, center.Y), drawFormat);
+
+
+            g.DrawLine(a, center.X, 10, center.X + 5, 20);
+            g.DrawLine(a, center.X, 10, center.X - 5, 20);
+            g.DrawLine(a, sizeWidth - 25, center.Y, sizeWidth - 30, center.Y + 5);
+            g.DrawLine(a, sizeWidth - 25, center.Y, sizeWidth - 30, center.Y - 5);
+            int stepForAxes = 25;
+            int lenghtShtrih = 3;
+            int maxValueForAcesX = 12;
+            int maxValueForAcesY = 10;
+            float oneDelenieX = (float)maxValueForAcesX / ((float)center.X / (float)stepForAxes);
+            float oneDelenieY = (float)maxValueForAcesY / ((float)center.Y / (float)stepForAxes);
+
+
+            for (int i = center.X, j = center.X, k = 1; i < 2 * center.X - 30; j -= stepForAxes, i += stepForAxes, k++)
+            {
+
+                g.DrawLine(a, i, center.Y - lenghtShtrih, i, center.Y + lenghtShtrih);
+
+                g.DrawLine(a, j, center.Y - lenghtShtrih, j, center.Y + lenghtShtrih);
+                if (i < 2 * center.X - 55)
+                {
+                    g.DrawString((k * oneDelenieX).ToString("0.0"), signatureFont, drawBrush, new PointF(i + stepForAxes + 9, center.Y + 6), drawFormat);
+                    g.DrawString(((k * oneDelenieX).ToString("0.0").ToString() + "‐"), signatureFont, drawBrush, new PointF(j - stepForAxes + 9, center.Y + 6), drawFormat);
+                }
+            }
+
+            for (int i = center.Y, j = center.Y, k = 1; i < 2 * center.Y - 30; j -= stepForAxes, i += stepForAxes, k++)
+            {
+
+                g.DrawLine(a, center.X - lenghtShtrih, i, center.X + lenghtShtrih, i);
+
+                g.DrawLine(a, center.X - lenghtShtrih, j, center.X + lenghtShtrih, j);
+                if (i < 2 * center.Y - 55)
+                {
+                    g.DrawString((k * oneDelenieY).ToString("0.0"), signatureFont, drawBrush, new PointF(center.X + 22, j + stepForAxes + 9), drawFormat);
+                    g.DrawString(((k * oneDelenieY).ToString("0.0").ToString() + "‐"), signatureFont, drawBrush, new PointF(center.X + 22, i - stepForAxes + 9), drawFormat);
+                }
+            }
+            int numOfPoint = 200;
+            float[] first = new float[numOfPoint];
+            for (int i = 0; i < numOfPoint; i++)
+            {
+                first[i] = (float)maxValueForAcesX / (float)numOfPoint * (i + 1) - (float)(maxValueForAcesX / 2);
+            }
+            float[] second = new float[numOfPoint];
+
+            for (int i = 0; i < numOfPoint; i++)
+            {
+                second[i] = (float)(Math.Exp(first[i]) * Math.Pow(first[i], 3));
+            }
+            Point[] pointOne = new Point[numOfPoint];
+            float tempX = 1 / oneDelenieX * stepForAxes;
+            float tempY = 1 / oneDelenieY * stepForAxes;
+            for (int i = 0; i < numOfPoint; i++)
+            {
+                pointOne[i].X = center.X + (int)(first[i] * tempX);
+
+                pointOne[i].Y = center.Y - (int)(second[i] * tempY);
+            }
+
+            g.DrawCurve(b, pointOne);
+            g.DrawLines(b, pointOne);
+        }
+        private void line()
+        {
+            chart1.Series[0].Points.Clear();
+            for (double x = -6; x <= 2; x += 0.01)
+            {
+                chart1.Series[0].Points.AddXY(x, Math.Exp(x) * Math.Pow(x, 3));
+            }
+        }
+    }
+}
